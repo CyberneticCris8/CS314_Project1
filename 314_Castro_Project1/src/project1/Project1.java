439357ed9cbde264bb90306632193d81f628b0af
Cris Castro <crcastro408@gmail.com>
2024-01-10 21:30:21 -0500

    V2

--- 314_Castro_Project1/src/project1/Project1.java
+++ 314_Castro_Project1/src/project1/Project1.java
@@ -5,110 +5,102 @@
  * Description: Program that takes 10x10 0-1 matrix and identifies it's properties (Reflexive, AntiReflexive, Symmetric, AntiSymmetric)
  */
 package project1;
-
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class Project1 {
-	/*
-public static int[][] matrix(int [][] maxFile) throws FileNotFoundException {
 
+public static boolean reflexive(int[][] refMatrix) {
+	for (int i = 0; i < refMatrix.length; i++) {
+		if (refMatrix[i][i] != 1) {
+			return false;
+		}
+	} 
+	return true; 
+} 	
+
+public static boolean antiReflexive(int[][] antiRefMatrix) {
+	for (int i = 0; i < antiRefMatrix.length; i++) {
+		if (antiRefMatrix[i][i] != 0) {
+				return false;
+			}
+	}	
+	return true; 
+}
+
+public static boolean symmetric(int[][] symMartix) { 
+	for (int i = 0; i < symMartix.length; i++) {
+		for (int j = 0; j < symMartix.length; j++) {
+			if (symMartix[i][j] != symMartix[j][i]) {
+				return false;
+			} 
+		}
+	}
+	return true;
+}
+public static boolean antiSymmetric(int[][] antiSymMartix) {
+	for (int i = 0; i < antiSymMartix.length; i++) {
+		for (int j = 0; j < antiSymMartix.length; j++) {
+			if (antiSymMartix[i][j] == antiSymMartix[j][i]) {
+				return false;
+			} 
+		}
+	}
+	return true; 
+}
+
+public static void main(String[] args) {					
 	Scanner input = new Scanner(System.in);
 	System.out.println("File input: ");
 	String fileName = input.nextLine();
 	File matrx = new File(fileName);
-	Scanner matrxFile = new Scanner(matrx); 
-
-	int matrixFile[][] = new int[10][10]; 
-	for (int i = 0; i < matrixFile.length; i++) {
-		for (int j = 0; j < matrixFile.length; j++) {
-			matrixFile[i][j] = matrxFile.nextInt(); 
+	Scanner matrxFile;
+	try {
+		matrxFile = new Scanner(matrx);
+		int[][] matrixFile = new int[10][10]; 
+		for (int i = 0; i < matrixFile.length; i++) { 
+			for (int j = 0; j < matrixFile.length; j++) {
+				if (matrxFile.hasNextInt()) {
+					matrixFile[i][j] = matrxFile.nextInt();
+				} 
+			}
+		}
+		if (reflexive(matrixFile)) {
+			System.out.println("Reflexive - yes");
+		} else {
+			System.out.println("Reflexive - no");
+		}
+		if (antiReflexive(matrixFile)) {
+			System.out.println("Antireflexive - yes");
+		} else {
+			System.out.println("Antireflexive - no");
+		}
+		if (symmetric(matrixFile)) {
+			System.out.println("Symmetric - yes");
+		} else {
+			System.out.println("Symmetric - no");
+		}
+		if (antiSymmetric(matrixFile)) {
+			System.out.println("Antisymmetric - yes");
+		} else {
+				System.out.println("Antisymmetric - no");
+		}
+	} 
+	
+	 catch (FileNotFoundException e) {
+		System.out.println("Input File Not Found");
+		}
+	} 
+}
 			
-		}
-	}
-	System.out.println(matrixFile);
-	return maxFile;
-
-}
-*/
-public static int[][] reflexive(int[][] refMatrix) {
-	int[][] refMatrx; 
-		for (int i = 0; i < refMatrx.length; i++) {
-			for (int j = 0; j < refMatrx.length; j++) {
-				if (refMatrx[i][j] == 1 && refMatrx[j][i] == 1) {
-					return refMatrx;
-				} else {
-					System.out.println("Reflexive - error");
-				}
-			}	
-		}
-	return refMatrix;
 		
-}
-public static int[][] antiReflexive(int[][] antiRefMatrix) {
-	int[][] antiMatrx; 
-		for (int i = 0; i < antiMatrx.length; i++) {
-			for (int j = 0; j < antiMatrx.length; j++) {
-				if (antiMatrx[i][j] != 1 && antiMatrx[j][i] != 1) {
-				return antiMatrx;
-				} else {
-				System.out.println("AntiReflexive - error");
-			}
-		}	 
-	}
-	return antiRefMatrix;
-}
-
-public static int[][]symmetric(int[][] symMartix) {
-	int[][] symMatrx; 
-		for (int j = 0; j < symMatrx.length; j++) {
-			for (int i = 0; i < symMatrx.length; i++) {
-				if (symMatrx[i][j] == symMatrx[j][i]) {
-				return symMatrx;
-			} else {
-				System.out.println("symmetric - error");
-			}
-		}
-	}
-	return symMartix;
-}
-public static int [][]antiSymmetric(int[][] antiSymMartix) {
-	int[][] antiSymMatrx; 
-		for (int j = 0; j < antiSymMatrx.length; j++) {
-			for (int i = 0; i < antiSymMatrx.length; i++) {
-				if (antiSymMatrx[i][j] != antiSymMatrx[j][i]) {
-				return antiSymMatrx;
-			} else {
-				System.out.println("symmetric - error");
-			}
-		}
-	}
-	return antiSymMartix;
-}
-
-public static void main(String[] args) throws FileNotFoundException {
-	
-	Scanner input = new Scanner(System.in);
-	System.out.println("File input: ");
-	String fileName = input.nextLine();
-	File matrx = new File(fileName);
-	Scanner matrxFile = new Scanner(matrx); 
-
-	int[][] matrixFile = new int[10][10]; 
-	for (int i = 0; i < matrixFile.length; i++) {
-		for (int j = 0; j < matrixFile.length; j++) {
-			matrixFile[i][j] = matrxFile.nextInt(); 
-			int[][] resultMat = reflexive(matrixFile[i][j]); 
-		}
-	}
-	
-	
 	
 
-}
 
-}
+
+
+
 
 	
 
